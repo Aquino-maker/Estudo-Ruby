2be0e3a06f9deb430ae23b5f29baa07fe8c01b93
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-07-16 11:19:36 -0300

    feat: adiciona exemplos de estruturas condicionais com if, elsif e else em Ruby

---
+++ condicionais/if.rb
@@ -0,0 +1,19 @@
+# x = 50
+
+# if x == 10
+#     puts "x é igual a 10"
+# elsif x == 50
+#     puts "x é igual a 50"
+# else
+#     puts "x não é igual a 10."
+# end
+
+name = gets.chomp
+
+if name == "Luan"
+    puts "Seu nome é bem bonito, se orgulhe disso"
+elsif name == "Guilherme"
+    puts "Hmmmm... Que nome generico"
+else
+    puts "Que nome mais... Estranho."
+end
