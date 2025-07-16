179a8435c674ec07c6e5274a5329070d1e1fd662
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-07-16 13:40:30 -0300

    feat: utiliza estrutura case para exibir quantidade de dias dos meses em Ruby

---
+++ condicionais/case.rb
@@ -0,0 +1,13 @@
+print "Digite o nome de um mês: "
+month = gets.chomp
+
+case month
+when "janeiro"
+    puts "tem 31 dias"
+when "fevereiro"
+    puts "tem 28 dias"
+when "março"
+    puts "tem 30 dias"
+else
+    puts "não sei quantos dias esse mês tem"
+end
